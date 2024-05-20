ffe11bdc2f47a9b6deaef3ce815d8a98ca677d57
enboz21 <enesb33@outlook.com>
2024-05-20 18:59:23 +0300

    düzeltme

--- veri/veri_olusturma/src/coğaltıcı.java
+++ veri/veri_olusturma/src/coğaltıcı.java
@@ -3,11 +3,9 @@ import java.time.LocalDate;
 
 public class coğaltıcı {
     public static void main(String[] args) throws Menü.hata {
-        /*for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < 4; i++) {
             Main m=new Main(i);
             System.out.println("tuurrr");
-        }*/
-        String masaustuDizini = System.getenv("USERPROFILE");
-        System.out.println(masaustuDizini);
+        }
     }
 }
